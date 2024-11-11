15c1c4925ecc61c24db78ea9ad57f3cdfe144be3
Lomemoraine <lorrainechepkemoi2001@gmail.com>
2024-11-11 12:35:55 +0300

    setup  myapp urls in the settings

--- PerlProject/urls.py
+++ PerlProject/urls.py
@@ -19,6 +19,6 @@ from django.urls import include ,path
 
 
 urlpatterns = [
-    path("myapp/", include("myapp.urls")),
+    path('', include("myapp.urls")),
     path('admin/', admin.site.urls),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -6,8 +6,8 @@ from django.shortcuts import render
 
 def index(request):
    return render(request, 'index.html')
-def category(request):
-   return render(request, 'category.html')
-def contact(request):
-   return render(request, 'contact.html')
+# def category(request):
+#    return render(request, 'category.html')
+# def contact(request):
+#    return render(request, 'contact.html')
 
